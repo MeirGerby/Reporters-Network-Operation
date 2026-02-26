9a95561d21a6a3b5d1c8b114161e9607a164cf54
MEIRG <meirg2001@gmail.com>
2026-02-26 17:34:58 +0200

    add producer and consumer in manager class

--- analytics_service/container.py
+++ analytics_service/container.py
@@ -8,5 +8,9 @@ class Manager:
         self.analysed_topic = settings.ANALYTICS_TEXT_KAFKA_TOPIC
         self.bootstrap = settings.ANALYTICS_SERVICE_BOOTSTRAP_SERVERS 
         self.group_id = settings.ANALYTICS_SERVICE_GROUP_ID 
+        self.producer: KafkaProducerWrapper = None 
+        self.consumer: KafkaConsumerWrapper = None 
+    
+    
 
-        
+    def set_up(self):
