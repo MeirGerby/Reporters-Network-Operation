fc1db982d15ff9aef4b892556c7051fc6be1f499
MEIRG <meirg2001@gmail.com>
2026-02-26 16:15:54 +0200

    initialize the kafka producer class

--- analytics_service/messaging.py
+++ analytics_service/messaging.py
@@ -1,4 +1,11 @@
 from confluent_kafka import Producer, Consumer 
 import json 
 
+class KafkaProducerWrapper:
+    """kafka producer class"""
+    def __init__(self, bootstrap_servers: str, topic: str):
+        self.config = {"bootstrap.servers":bootstrap_servers }
+        self.producer = Producer(self.config)   # type: ignore 
+        self.topic = topic 
 
+    
