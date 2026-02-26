d53d11e3f6b56e4f8bd4b6056aabc0b6835a1ea6
MEIRG <meirg2001@gmail.com>
2026-02-26 23:53:34 +0200

    add find weapons method for finding if there is in the text

--- analytics_service/text_analizer.py
+++ analytics_service/text_analizer.py
@@ -5,10 +5,20 @@ class TextAnalyzer:
     def __init__(self, text: str):
         self.text = text 
         self.counter = Counter
-        self.split_words = self.text.split()
+        self.split_words = self.text.split() 
 
     def count_words(self) -> List[tuple[str, int]]:
+        """find the most common words in the text"""
         count_words = self.counter(self.split_words)  # type: ignore
         return self.counter.most_common(count_words)
 
+    def find_weapens(self, weapon_file):
+        """find weapon if they are in the text """
+        weapon_list = []
+        with open(weapon_file, 'r') as f:
+            data = f.read() 
+            for i in data.split():
+                if i in self.text:
+                    weapon_list.append(i)
+        return weapon_list 
 
