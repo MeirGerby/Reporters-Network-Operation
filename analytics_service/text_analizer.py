5314645171bec8ba6d55f240bac2ec7089840147
MEIRG <meirg2001@gmail.com>
2026-02-26 23:33:01 +0200

    count words function return the most common words

--- analytics_service/text_analizer.py
+++ analytics_service/text_analizer.py
@@ -1,4 +1,14 @@
-
+from collections import Counter 
+from typing import List
 
 class TextAnalyzer:
-    pass 
+    def __init__(self, text: str):
+        self.text = text 
+        self.counter = Counter
+        self.split_words = self.text.split()
+
+    def count_words(self) -> List[tuple[str, int]]:
+        count_words = self.counter(self.split_words)  # type: ignore
+        return self.counter.most_common(count_words)
+
+
